280c528dbb707eb55909a6b77b14c890c57bde3d
Andrey-V-83 <mallpall@mail.ru>
2022-06-01 11:52:38 +0300

    Задание (обьекты) выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -18,48 +18,47 @@ student2.setSubject("Geometry");
 
 let arrMarks = [];
 
-Student.prototype.addMark = function (mark) {  
-  
+Student.prototype.addMark = function (mark) {
+	
+  arrMarks.push(mark);
   
   if(this.marks === undefined){    
     this.marks = mark;
-    
-  } else {
-    
-    arrMarks.push(mark);
+  } else {        
     this.marks = arrMarks;
   }
   
 } 
    
-student1.addMark(5);
-student1.addMark(4); 
-student1.addMark(5);
+//student1.addMark(5);
+//student1.addMark(4); 
+//student1.addMark(5);
 
 
-let arrMarks2 = [];
-this.marks = arrMarks2;
+
+
 Student.prototype.addMarks = function (mark1, mark2, ...rest) {
-  arrMarks2.push(mark1, mark2, ...rest);         
-  
+  let arrMarks2 = [];
+  arrMarks2.push(mark1, mark2, ...rest);  
+  this.marks = arrMarks2;
 }
 
-student2.addMarks(2, 3, 2);
+//student2.addMarks(2, 3, 2);
 
 Student.prototype.getAverage = function () {
   return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
 }
 
-console.log(student1.getAverage());
-console.log(student2.getAverage());
+//console.log(student1.getAverage());
+//console.log(student2.getAverage());
 
 Student.prototype.exclude = function (reason) {
-  delete this.subject;
+  delete this.subject; 
   delete this.marks;
   this.excluded = reason;
 }
 
-student2.exclude('low grades');
+//student2.exclude('low grades');
 
-console.log(student1);
-console.log(student2);
+//console.log(student1);
+//console.log(student2);
