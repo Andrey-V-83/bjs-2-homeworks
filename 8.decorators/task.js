5e373d4dea26a6e9896c92b4b16a35c46cdad626
Andrey-V-83 <mallpall@mail.ru>
2022-06-17 10:13:31 +0300

    Задание выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,27 @@
+'use strict';
+
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+  let cache = [];
+  
+  function wrapper(...args) {
+    const hash = args.join(','); // получаем правильный хэш
+    let idx = cache.findIndex((item)=> item.hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
+    if (idx !== -1 ) { // если элемент найден
+      console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, получаем нужное значение
+      return "Из кэша: " + cache[idx].result;
+    }
+  
+    let result = func(...args); // в кэше результата нет - придётся считать
+    cache.push({hash, result}); // добавляем элемент с правильной структурой
+    if (cache.length > 5) { 
+      cache.shift(); // если слишком много элементов в кэше, удаляем самый старый (первый) 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
+
+  return wrapper;
+  }
 
 
 function debounceDecoratorNew(func) {
